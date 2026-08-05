922bf31b640a2cce2a1839afdc24f6339d0a0f24
2nhgp6bksm-hue <2nhgp6bksm@privaterelay.appleid.com>
2026-08-05 20:34:38 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE='gomo-vs-planner-v3.10.0';
+const CACHE='gomo-vs-planner-v3.11.0';
 const ASSETS=[
   './','./index.html','./styles.css?v=2.26.0','./app.js?v=2.26.0',
-  './upgrade-v2.30.js','./upgrade-v2.31.js','./upgrade-v2.32.js','./upgrade-v2.40.js','./upgrade-v2.41.js','./upgrade-v2.42.js','./upgrade-v3.00.js','./upgrade-v3.10.js',
+  './upgrade-v2.30.js','./upgrade-v2.31.js','./upgrade-v2.32.js','./upgrade-v2.40.js','./upgrade-v2.41.js','./upgrade-v2.42.js','./upgrade-v3.00.js','./upgrade-v3.10.js','./upgrade-v3.11.js',
   './manifest.webmanifest','./icon-192.png','./icon-512.png','./gomo-vs-planner-v2.42.png?v=2.42.0'
 ];
 
@@ -16,7 +16,8 @@ const MARKERS=[
   ['data-gomo-v241','./upgrade-v2.41.js'],
   ['data-gomo-v242','./upgrade-v2.42.js'],
   ['data-gomo-v300','./upgrade-v3.00.js'],
-  ['data-gomo-v310','./upgrade-v3.10.js']
+  ['data-gomo-v310','./upgrade-v3.10.js'],
+  ['data-gomo-v311','./upgrade-v3.11.js']
 ];
 
 async function decorateHtml(response){
