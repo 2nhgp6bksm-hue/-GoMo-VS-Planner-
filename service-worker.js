8b7ca333a907ddf3d2e85d0269d0da1fea3d18c6
2nhgp6bksm-hue <2nhgp6bksm@privaterelay.appleid.com>
2026-08-19 00:04:57 +0200

    v3.18 update PWA cache for weekly history

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='gomo-vs-planner-v3.17.1';
+const CACHE='gomo-vs-planner-v3.18.0';
 
 const ASSETS=[
   './',
@@ -13,8 +13,9 @@ const ASSETS=[
   './upgrade-v2.42.js?v=3.15.0',
   './upgrade-v3.15.js?v=3.15.0',
   './upgrade-v3.16.js?v=3.16.0',
-  './upgrade-v3.16.1.js?v=3.17.1',
-  './upgrade-v3.17.js?v=3.17.1',
+  './upgrade-v3.16.1.js?v=3.18.0',
+  './upgrade-v3.17.js?v=3.17.2',
+  './upgrade-v3.18.js?v=3.18.0',
   './manifest.webmanifest',
   './icon-192.png',
   './icon-512.png',
