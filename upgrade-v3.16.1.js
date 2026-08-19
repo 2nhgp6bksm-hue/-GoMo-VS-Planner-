d9316daf7a70664d35225c7b865dd8da2912b55c
2nhgp6bksm-hue <2nhgp6bksm@privaterelay.appleid.com>
2026-08-19 11:09:51 +0200

    Load final post-test translation patch

--- upgrade-v3.16.1.js
+++ upgrade-v3.16.1.js
@@ -67,6 +67,7 @@
     loadScript('data-gomo-v3192-loader','data-gomo-v3192-ready','./upgrade-v3.19.2.js?v=3.19.2');
     loadScript('data-gomo-i18nfix-loader','data-gomo-i18n-fix','./upgrade-v3.20.5-i18n.js?v=3.20.5-i18n1');
     loadScript('data-gomo-i18nfinal-loader','data-gomo-i18n-final','./upgrade-v3.20.5-i18n-final.js?v=3.20.5-i18n3');
+    loadScript('data-gomo-i18nposttest-loader','data-gomo-i18n-posttest','./upgrade-v3.20.5-i18n-posttest.js?v=3.20.5-i18n3');
   }
 
   function clean(){
